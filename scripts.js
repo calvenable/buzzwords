97c83560e63282441628453832482069ce4e7eca
Caleb Venable <calvenable@gmail.com>
2023-10-23 13:54:07 +0100

    added random letter and category generator

--- scripts.js
+++ scripts.js
@@ -1,6 +1,32 @@
-const letters = [];
-var frequencies = [];
+// Generator functions and properties
+const letters = ['T', 'A', 'O', 'I', 'S', 'W', 'C', 'B', 'P', 'H', 'F', 'M', 'D', 'E', 'R', 'L', 'N', 'G', 'U', 'K', 'V', 'Y', 'J', 'Q', 'X', 'Z'];
+const probs = [.16, .117, .076, .073, .067, .055, .052, .044, .043, .042, .04, .038, .032, .028, .028, .024, .023, .016, .012, .0086, .0082, .0076, .0051, .0022, .00045, .00045];
+let aggProbs = [];
+function initialiseProbs() {
+    let count = 0.0;
+    for (let i = 0; i < probs.length; i++) {
+        count += probs[i];
+        aggProbs[i] = count;
+    }
+}
 
+function generateLetter() {
+    var randValue = Math.random();
+    var index = 0;
+    while (aggProbs[index] < randValue) {
+        index++;
+    }
+    return letters[index];
+};
+
+
+function generateCategory() {
+    return categories[Math.floor(Math.random() * categories.length)];
+}
+
+
+
+// Actual program functionality
 const State = {
     Menu: 1,
     Intro: 2,
@@ -9,6 +35,7 @@ const State = {
     Answers: 5
 };
 let pageState = State.Menu;
+initialiseProbs();
 
 document.addEventListener("keypress", (event) => {handleKeyPress(event);});
 
@@ -18,23 +45,71 @@ function handleKeyPress(event) {
         case " ":
             advancePageState();
         break;
-    default:
-        console.log("Pressed " + event.key);
+        default:
+            console.log("Letter: " + generateLetter());
+            console.log("Category: " + generateCategory());
   }
 }
 
 function advancePageState() {
     switch (pageState) {
         case State.Menu:
-            document.getElementById("buzz").classList.add("slideLeft");
-            document.getElementById("words").classList.add("slideRight");
+            titleVisible(false);
+            hideElementsIn("footer");
             pageState = State.Intro;
             break;
         case State.Intro:
-            document.getElementById("buzz").classList.remove("slideLeft");
-            document.getElementById("words").classList.remove("slideRight");
+            titleVisible(true);
+            showElementsIn("footer");
             pageState = State.Menu;
             break;
 
     }
 }
+
+function titleVisible(state) {
+    if (state) {
+        document.getElementById("buzz").classList.remove("slideLeft");
+        document.getElementById("words").classList.remove("slideRight");
+    }
+    else {
+        document.getElementById("buzz").classList.add("slideLeft");
+        document.getElementById("words").classList.add("slideRight");
+    }
+}
+
+function showElementsIn(divID) {
+    elements = document.getElementById(divID).children;
+    [...elements].forEach(item => {
+        item.classList.remove("hidden");
+    });
+}
+
+function hideElementsIn(divID) {
+    elements = document.getElementById(divID).children;
+    [...elements].forEach(item => {
+        item.classList.add("hidden");
+    });
+}
+
+
+
+let categories = [
+    'fruits',
+    'items of clothing',
+    'countries',
+    'British cities',
+    'items of clothing',
+    'animals',
+    'body parts',
+    'car makes/models',
+    'things you\'d find in a kitchen',
+    'things you\'d find in a park',
+    'famous actors (surname)',
+    'cartoon characters',
+    'colours',
+    'items of furniture',
+    'beverages (drinks)',
+    'foods',
+    'sports',
+    'books of the Bible'];

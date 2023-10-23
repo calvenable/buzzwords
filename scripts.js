77ab7351ff2fb3b5255042bf616e847172c5ba86
Caleb Venable <calvenable@gmail.com>
2023-10-23 10:41:00 +0100

    added title transitions and gamestate

--- scripts.js
+++ scripts.js
@@ -1,17 +1,40 @@
 const letters = [];
 var frequencies = [];
 
+const State = {
+    Menu: 1,
+    Intro: 2,
+    Waiting: 3,
+    RoundInProgress: 4,
+    Answers: 5
+};
+let pageState = State.Menu;
+
 document.addEventListener("keypress", (event) => {handleKeyPress(event);});
 
 function handleKeyPress(event) {
-  switch (event.Key) {
-    case "Enter":
-      console.log("Pressed enter!");
-      break;
-    case "Space":
-      console.log("Pressed space!");
-      break;
+    switch (event.key) {
+        case "Enter":
+        case " ":
+            advancePageState();
+        break;
     default:
-      console.log("Pressed " + event.Key);
+        console.log("Pressed " + event.key);
   }
+}
+
+function advancePageState() {
+    switch (pageState) {
+        case State.Menu:
+            document.getElementById("buzz").classList.add("slideLeft");
+            document.getElementById("words").classList.add("slideRight");
+            pageState = State.Intro;
+            break;
+        case State.Intro:
+            document.getElementById("buzz").classList.remove("slideLeft");
+            document.getElementById("words").classList.remove("slideRight");
+            pageState = State.Menu;
+            break;
+
+    }
 }
